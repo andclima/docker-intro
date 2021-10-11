5f0ebfa0b3eb78865d37b06adbb13e1d6afefc2b
Anderson Lima <andclima@gmail.com>
2021-10-11 19:44:03 -0300

    Versão inicial

---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int count = 1;
+        try {
+            while (true) {
+                Thread.sleep(2 * 1000);
+                System.out.println("Counter: " + count++);
+    
+            }
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
